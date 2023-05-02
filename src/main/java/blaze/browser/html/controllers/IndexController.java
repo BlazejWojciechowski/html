e02df5f37116d59ddb7ac36a434355570f8bee8b
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-02 14:47:01 +0200

    added index.html and IndexController for home page

---
+++ src/main/java/blaze/browser/html/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package blaze.browser.html.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/" , "/home", "/home/"})
+public class IndexController {
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+}
